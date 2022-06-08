17b596b8a5180f4924b9b00d0e5dde29104ba129
nievein ahmed <nevenahmed039@gmail.com>
2022-06-08 13:11:18 +0200

    updating

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+const arr = [];
 const ul = document.querySelector("ol");
 const addToList = (e) => {
   e.preventDefault();
@@ -10,15 +10,31 @@ const addToList = (e) => {
     let text = document.createTextNode(todo);
     newLi.appendChild(text);
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "❌";
+    const doneBtn = document.createElement("button");
+    doneBtn.innerHTML = "✔️";
+    delBtn.innerHTML = " ❌ ";
+    newLi.appendChild(doneBtn);
+    
     newLi.appendChild(delBtn);
     ul.appendChild(newLi);
+    doneBtn.addEventListener("click", () => newLi.classList.add("done"));
+    
+
     delBtn.addEventListener("click", () => newLi.remove());
     document.querySelector("#todo").value = "";
+    
   } else {
     
   }
 };
 
 document.querySelector("form").addEventListener("submit", addToList);
+$(document).on("click", ".check", function() {
+
+  $(this).toggleClass("selected");
+
+});
+
+// When enter key is pressed append a new item to the list
+
 
